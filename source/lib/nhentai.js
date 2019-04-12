8ba82c213ed863fc8ec6945c5bf08bfe33d09c5e
Mino Zang <minoscc+dev@gmail.com>
2019-04-12 14:49:37 +0800

    更新图片地址

--- source/lib/nhentai.js
+++ source/lib/nhentai.js
@@ -11,7 +11,12 @@ module.exports = async input => {
 
     const title = $('#info > h1').text();
     const images = $('#thumbnail-container img.lazyload')
-        .map((_, ele) => $(ele).data('src'))
+        .map((_, ele) =>
+            $(ele)
+                .data('src')
+                // 1t.jpg 是缩略图
+                .replace(/([0-9]{1,})t/g, '$1')
+        )
         .toArray();
 
     return {
