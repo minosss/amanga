852a5bd2203d2daeddc125e0ee541e772bb2e576
Mino Zang <minoscc+dev@gmail.com>
2020-06-25 17:27:19 +0800

    添加QQ

---
+++ __tests__/qq.test.ts
@@ -0,0 +1,10 @@
+import amanga from '../source';
+
+describe('QQ', () => {
+	test('海贼王 第195话', async () => {
+		const manga = await amanga('https://ac.qq.com/ComicView/index/id/505430/cid/196');
+
+		expect(manga.title).toBe('航海王');
+		expect(manga.chapter).toBe('第195话 Mr.武士道');
+	});
+});
